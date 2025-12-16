afcc00497cfd9f750a99ae7e4f512cb923b44746
Kear Ibale <kdwest62@gmail.com>
2025-12-16 15:10:17 +0800

    Add function to convert pounds to kilograms

---
+++ convert-to-kgs.py
@@ -0,0 +1,10 @@
+def convert_to_kgs (lbs):
+    kgs = round((lbs * 0.453592), 2)
+    if lbs == 1 and kgs !=1:
+        return f"{lbs} pound equals to {kgs} kilograms."
+    elif kgs == 1 and lbs != 1:
+        return f"{lbs} pounds equals to {kgs} kilogram."
+    elif kgs == 1 and lbs == 1:
+        return f"{lbs} pound equals to {kgs} kilogram."
+    else:
+        return f"{lbs} pounds equals to {kgs} kilograms."
