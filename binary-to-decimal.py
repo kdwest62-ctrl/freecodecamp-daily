0f14d414745461d39dc58f20459f730111c4d00e
Kear Ibale <kdwest62@gmail.com>
2025-12-20 09:51:08 +0800

    Validate binary input in to_decimal function
    Add validation for binary input in to_decimal function.

--- binary-to-decimal.py
+++ binary-to-decimal.py
@@ -1,8 +1,17 @@
 def to_decimal(binary):
-    num_list = []
-    exp = len(str(binary)) - 1
+    binary_check = []
     for item in str(binary):
-        num = int(item) * (2 ** exp)
-        num_list.append(num)
-        exp -= 1
-    return sum(num_list)
+        if item != "0" and item != "1":
+            binary_check.append(item)
+        else:
+            continue
+    if len(binary_check) != 0:
+        return "A binary number can only be made up of 0's and 1's"
+    else:
+        num_list = []
+        exp = len(str(binary)) - 1
+        for item in str(binary):
+            num = int(item) * (2 ** exp)
+            num_list.append(num)
+            exp -= 1
+        return sum(num_list)
