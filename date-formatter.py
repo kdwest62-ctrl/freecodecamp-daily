e06d2f7e82ed80680b71f9b3657206a0e9799de7
Kear Ibale <kdwest62@gmail.com>
2025-12-31 14:05:09 +0800

    Refactor date formatting logic and validation

--- date-formatter.py
+++ date-formatter.py
@@ -1,6 +1,12 @@
-import string
-
 def format_date(date_string):
+    month_dict = {
+        'January': '01', 'February': '02', 'March': '03',
+        'April': '04', 'May': '05', 'June': '06',
+        'July': '07', 'August': '08', 'September': '09',
+        'October': '10', 'November': '11', 'December': '12'}
+    def year(date):
+        year_char = [date[-4], date[-3], date[-2], date[-1]]
+        return "".join(year_char)
     def month(date):
         month_char = []
         for item in date:
@@ -15,18 +21,16 @@ def format_date(date_string):
             if element == ",":
                 break
             else:
-                if element in string.digits:
+                if element in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
                     day_char.append(element)
                 else:
                     continue
         return "".join(day_char)
-    def year(date):
-        year_char = [date[-4], date[-3], date[-2], date[-1]]
-        return "".join(year_char)
-    if len(month(date_string)) > 2:
-        if len(day(date_string)) < 2:
-            return f"{year(date_string)}-{month(date_string)}-0{day(date_string)}"
-        else:
-            return f"{year(date_string)}-{month(date_string)}-{day(date_string)}"
+
+    if month(date_string) not in month_dict.keys() or int(day(date_string)) > 31:
+        return "Invalid input"
     else:
-        return "Input full name of the month"
+        if len(day(date_string)) < 2:
+            return f"{year(date_string)}-{month_dict[month(date_string)]}-0{day(date_string)}"
+        else:
+            return f"{year(date_string)}-{month_dict[month(date_string)]}-{day(date_string)}"
